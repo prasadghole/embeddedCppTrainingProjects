f8ee83e40a0bf99fb70c351ceb063f244494a93c
Prasad Ghole <prasad.ghole@gmail.com>
2019-08-18 13:17:58 +0530

    Both UART 2 and 3 implemented

---
+++ SerialPort_Interface_AllUart/Inc/Uart3.h
@@ -0,0 +1,35 @@
+#ifndef INC_Uart3_H_
+#define INC_Uart3_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif /* __cplusplus */
+
+#include "Iusart.h"
+#include "stm32f7xx_hal.h"
+
+  namespace stm32f767
+  {
+    namespace Usart
+    {
+      class Uart3: public IUsart
+      {
+        public:
+          Uart3();
+          Uart3(Settings & settings);
+
+          virtual bool Initialize() override;
+          virtual bool Transmit(const char * buffer, const uint32_t len) override;
+        private:
+          UART_HandleTypeDef m_handle;
+          Settings m_setting;
+      };
+
+    }
+  }
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* INC_UART_H_ */
---
+++ SerialPort_Interface_AllUart/Readme.md
@@ -0,0 +1,7 @@
+Demonstrate how to use simple objects to encapsulte hardware details.
+
+Setup
+1. Connect nucleo-f767 board to PC.
+2. Hardware setup GND and CN9 pin 6
+3. Keep setting as 115200,7,N,1
+4. Observe banner "Hello World on Terminal"
